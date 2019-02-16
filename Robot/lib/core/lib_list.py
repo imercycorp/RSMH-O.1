e1ac73327773ba20914c2ad8f6ba7a3224a0f264
Rhenar <hugo.chassaing@orange.fr>
2019-02-16 10:48:23 +0100

    Version 0.1d
    Co-Authored-By: chevalley <chevalley@users.noreply.github.com>
    Co-Authored-By: Ruzand <ruzand@users.noreply.github.com>

--- Robot/lib/core/lib_list.py
+++ Robot/lib/core/lib_list.py
@@ -1,4 +1,6 @@
 class List:
 
-    def select(self, nexus_list, nb_servo): 
-        return nexus_list[nb_servo]
+    def select(self, nexus_list, nb_servo):
+        self.nexus_list = nexus_list
+        self.nb_servo = nb_servo 
+        return self.nexus_list[self.nb_servo]
